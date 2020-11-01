4151b1d17d0afa39040c4f3d797cc85558795e24
cameronmukherjee <j.t.1st@outlook.com>
2020-11-01 05:41:57 +0000

    Fixes #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, Navbar, Nav, FormControl, Form } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-// Testing automated kaban.
+// import _Navbar from './compontent/Navbar'
+import _Home from './compontent/Home'
+import _Login from './compontent/Login'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Navbar bg="light" expand="lg" style={{ marginBottom: 100 }}>
+            <Link to="/"><Navbar.Brand>Summarize</Navbar.Brand></Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    {/* <Link to="/top"><Button className="mr-sm-2">Top</Button></Link> */}
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success" className="mr-sm-2">Search</Button>
+                </Form>
+                <Link to="/login"><FontAwesomeIcon icon={ faUser } size={"2x"}></FontAwesomeIcon></Link>
+            </Navbar.Collapse>
+        </Navbar>
+
+      <Switch>
+        <Route path="/login">
+          <_Login />re
+        </Route>
+        <Route path="/">
+          <_Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/compontent/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
+function _Home() {
+    return (
+        <Container className="fluid">
+            <Row>
+                <Col className="my-auto">
+                    <h1 style={{ fontSize: 100 }}>
+                        Summarize
+                    </h1>
+                    <h3 className="text-center">
+                        your favourite books in 2500 words
+                    </h3>
+                </Col>
+                <Col className="my-auto">
+                <Image src="./book-lover.svg" className="img img-fluid"></Image>
+                </Col>
+            </Row>
+        </Container>  
+    );
+}
+
+export default _Home;
