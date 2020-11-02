79fdf47418c0dde057793520c2973e788901ea10
cameronmukherjee <j.t.1st@outlook.com>
2020-11-02 09:20:42 +0000

    Fixed #41

--- src/compontent/Feed.tsx
+++ src/compontent/Feed.tsx
@@ -2,9 +2,19 @@ import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Container, Row, Col, Image, Button, Form, FormGroup, FormLabel } from 'react-bootstrap';
 
+import firebase from 'firebase/app';
+// eslint-disable-next-line import/no-unassigned-import
+import 'firebase/firestore';
+// eslint-disable-next-line import/no-unassigned-import
+import 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
 function _Feed(props: { user: any; }) {
     const [bookTitle, setBookTitle] = useState("");
     const [summaryBody, setSummaryBody] = useState("");
+
+    const [ summaries ]: any[] = useCollectionData(firebase.firestore().collection('summary').orderBy('createdAt').limit(25), { idField: 'id' });
 
     if (!props.user) {
         return (<Redirect to="/login" />)
@@ -25,33 +35,46 @@ function _Feed(props: { user: any; }) {
                         <Col>
                             <h4>{props.user.displayName}</h4>
                             <h6>{props.user.email}</h6>
-                            <h6>Total summaries on system: 0</h6>
                         </Col>
                     </Row>
                 </Col>
-                <Col className="col col-md-8">
-                    <Summary />
+                <Col className="col col-md-8 scroll-col" style={{ maxHeight: "80vh", width:"100%"}}>
+                    { summaries && summaries.map((summary: {id: string}) => <Summary key={summary.id} message={summary}></Summary>) }
                 </Col>
             </Row>
         </Container>
     );
 }
 
-function Summary() {
+function Summary(props: { key: any, message: any}) {
+    const { content, uid, bookID, bookTitle, displayName, photoURL } = props.message;
+    // FIXME: Created at crashing application.
+
     return (
+        // <h1>{content}</h1>
         <Row>
             <Col>
-                <h2>1984 - George Orwell</h2>
+                <h2>{bookTitle}</h2>
                 <p>
-                    Main text, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non placeat doloremque praesentium fuga voluptatum, porro maiores aliquam dolores, modi ea tempora delectus enim at saepe quasi dicta fugit ipsum illum.
+                    {content}
                 </p>
-                <div className="text-right">
-                    <small> PROFILE PIC - Author Name</small>
-                </div>
-                <div className="text-right">
-                    <Button style={{ backgroundColor: "#6c63ff" }} className="mr-sm-1">Like</Button>
-                    <Button style={{ backgroundColor: "#6c63ff" }} >Comment</Button>
-                </div>
+                <Row>
+                    <Col className="col col-sm-1 my-auto">
+                    <a href={uid}><Image src={photoURL} style={{ height: 60 }} className="mr-sm-3" roundedCircle/></a>
+                    </Col>
+                    <Col className="col col-sm-5 my-auto">
+                    <div>
+                        <h6>{displayName}</h6>
+                    </div>
+                    </Col>
+                    <Col className="col col-sm-6">
+                        <div className="text-right">
+                            <Button style={{ backgroundColor: "#6c63ff" }} className="mr-sm-1">Like</Button>
+                            <Button style={{ backgroundColor: "#6c63ff" }} >Comment</Button>
+                        </div>
+                    </Col>
+                </Row>
+                <hr/>
             </Col>
         </Row>
     )
