17857a63d1c6cd57b55724f22cc9600f44edb65a
cameronmukherjee <j.t.1st@outlook.com>
2020-11-03 10:48:37 +0000

    created base setup for liking note

--- src/compontent/Feed.tsx
+++ src/compontent/Feed.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import * as axios from 'axios';
 import { Container, Row, Col, Image, Button, Form, FormGroup, FormLabel } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faComment } from '@fortawesome/free-solid-svg-icons';
 
 import firebase from 'firebase/app';
 // eslint-disable-next-line import/no-unassigned-import
@@ -9,6 +12,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import Axios from 'axios';
 
 function _Feed(props: { user: any; }) {
     if (!props.user) {
@@ -17,7 +21,7 @@ function _Feed(props: { user: any; }) {
     const [bookTitle, setBookTitle] = useState("");
     const [summaryBody, setSummaryBody] = useState("");
 
-    const [ summaries ]: any[] = useCollectionData(firebase.firestore().collection('summary').orderBy('createdAt').limit(25), { idField: 'id' });
+    const [summaries]: any[] = useCollectionData(firebase.firestore().collection('summary').orderBy('createdAt').limit(25), { idField: 'id' });
 
 
     return (
@@ -26,24 +30,29 @@ function _Feed(props: { user: any; }) {
                 <Col className="col-md-4 text-center d-none d-md-block d-lg-block">
                     <Image src={props.user.photoURL} roundedCircle />
                     <h4>{props.user.displayName}</h4>
-                    <h6>{props.user.email}</h6> 
-                    <Link to="/search"><Button style={{width: "100%", marginBottom: 10, backgroundColor: "#6c63ff"}}>Write Summary Now ✍️</Button></Link>
+                    <h6>{props.user.email}</h6>
+                    <Link to="/search"><Button style={{ width: "100%", marginBottom: 10, backgroundColor: "#6c63ff" }}>Write Summary Now ✍️</Button></Link>
                     <small>(Pskk, you can scroll on the text) ➡️</small>
                 </Col>
-                <Col className="scroll-col col-md-8" style={{ maxHeight: "85vh", msOverflowStyle: "none", scrollbarWidth: "none"}}>
+                <Col className="scroll-col col-md-8" style={{ maxHeight: "85vh", msOverflowStyle: "none", scrollbarWidth: "none" }}>
                     <div className="d-xs-block d-sm-block d-md-none ">
-                        <Link to="/search"><Button style={{width: "100%", marginBottom: 10, backgroundColor: "#6c63ff"}}>Write Summary Now ✍️</Button></Link>
+                        <Link to="/search"><Button style={{ width: "100%", marginBottom: 10, backgroundColor: "#6c63ff" }}>Write Summary Now ✍️</Button></Link>
                     </div>
-                    { summaries && summaries.map((summary: {id: string}) => <Summary key={summary.id} message={summary}></Summary>) }
+                    {summaries && summaries.map((summary: { id: string }) => <Summary key={summary.id} message={summary} user={props.user}></Summary>)}
                 </Col>
             </Row>
         </Container>
     );
 }
 
-function Summary(props: { key: any, message: any}) {
-    const { content, uid, bookID, bookTitle, displayName, photoURL, createdAt } = props.message;
+function Summary(props: { key: any, message: any, user: any }) {
+    const { content, uid, bookID, bookTitle, displayName, photoURL, createdAt, id } = props.message;
     // FIXME: Created at crashing application.
+
+    const likeSummary = () => {
+        const url = "";
+        // axios.post()
+    }
 
     return (
         // <h1>{content}</h1>
@@ -55,17 +64,25 @@ function Summary(props: { key: any, message: any}) {
                     {content}
                 </p>
                 <Row>
-                    <Col className="col col-sm-1 my-auto">
-                        <a href={uid}><Image src={photoURL} style={{ height: 60 }} className="mr-sm-5" roundedCircle/></a>
+                    <Col className="col my-auto">
+                        <a href={uid}><Image src={photoURL} style={{ height: 60 }} className="mr-sm-5" roundedCircle /></a>
                     </Col>
-                    <Col className="col col-sm-11 my-auto">
+                    <Col className="col my-auto">
                         <div className="text-right">
-                            <Button style={{ backgroundColor: "#6c63ff" }} className="mr-sm-1">Like</Button>
-                            <Button style={{ backgroundColor: "#6c63ff" }} >Comment</Button>
+                            <a onClick={() => console.log(id)}><FontAwesomeIcon icon={faHeart} size="2x" style={{ color: "#6c63ff" }} className="mr-sm-1"></FontAwesomeIcon></a>
+                            <FontAwesomeIcon icon={faComment} size="2x" style={{ color: "#6c63ff" }} className="mr-sm-1"></FontAwesomeIcon>
+                            {/* <Button style={{ backgroundColor: "#6c63ff" }} >Comment</Button> */}
                         </div>
                     </Col>
                 </Row>
-                <hr/>
+                <Row>
+                    <Col>
+                        <div className="text-right">
+                            <small>13 likes - 4 comments</small>
+                        </div>
+                    </Col>
+                </Row>
+                <hr />
             </Col>
         </Row>
     )
